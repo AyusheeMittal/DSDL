75e44ba391da5fb52816d7166e7b2a4dfb213eb2
Ayushee Mittal <ayusheemittal530@gmail.com>
2021-12-03 16:41:56 +0530

    Update coco.py
    printing paths

--- coco/coco.py
+++ coco/coco.py
@@ -13,10 +13,13 @@ urls = {'train_img':'http://images.cocodataset.org/zips/train2014.zip',
         'annotations':'http://images.cocodataset.org/annotations/annotations_trainval2014.zip'}
 
 def download_coco2014(root, phase):
+    print('root-',root)
     if not os.path.exists(root):
         os.makedirs(root)
     tmpdir = os.path.join(root, 'tmp/')
     data = os.path.join(root, 'data/')
+    print('tmpdir-',tmpdir)
+    print('data-',data)
     if not os.path.exists(data):
         os.makedirs(data)
     if not os.path.exists(tmpdir):
