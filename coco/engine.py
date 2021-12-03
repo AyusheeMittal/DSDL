d8409a48117b5bfb71d3427b2cd1a1f2ff81d2a7
Ayushee Mittal <ayusheemittal530@gmail.com>
2021-12-03 13:26:50 +0530

    Update engine.py
    fixing cuda(async=true) invalid syntax error

--- coco/engine.py
+++ coco/engine.py
@@ -241,7 +241,7 @@ class Engine(object):
             self.on_start_batch(True, model, criterion, data_loader, optimizer)
 
             if self.state['use_gpu']:
-                self.state['target'] = self.state['target'].cuda(async=True)
+                self.state['target'] = self.state['target'].cuda(non_blocking=True)
 
             self.on_forward(True, model, criterion, data_loader, optimizer)
             data_loader.set_postfix(loss = self.state['loss'].data)
